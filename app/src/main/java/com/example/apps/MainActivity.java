73e511bfe3b1f554c6546a0e424d5a75df9c2969
Marcelo <marcelo.ldf06@aluno.ifsc.edu.br>
2026-08-21 12:10:46 -0300

    n funcionando botao com numeros

--- app/src/main/java/com/example/apps/MainActivity.java
+++ app/src/main/java/com/example/apps/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.apps;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -29,13 +30,24 @@ public class MainActivity extends AppCompatActivity {
         Button b = findViewById(R.id.button);
         TextView tv = findViewById(R.id.textView);
 
-        b.setOnClickListener(view -> {
+        EditText edmin = findViewById(R.id.edmin);
+        EditText edmax = findViewById(R.id.edmax);
 
+        b.setOnClickListener(view -> {
+            int min = Integer.parseInt(edmin.getText().toString());
+            int max = Integer.parseInt(edmax.getText().toString());
             contador = contador + Math.round(sum);
             tv.setText(Double.toString(contador));
-            tv.setBackgroundColor(rng.nextInt());
+            tv.setBackgroundColor(rng.nextInt(min, max));
             tv.setTextSize(55);
-            sum = sum * 1.05;
+
+            if (contador >= 20*sum){
+                contador = contador - 20*sum;
+                sum = sum * 1.05;
+            }
+
+
+
         });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
